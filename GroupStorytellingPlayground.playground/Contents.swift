450c78602ff1088dd081e82b0862b0810e05c7f5
thodep <thodep@yahoo.com>
2015-08-12 18:32:11 -0400

    Tho’s Line

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -4,3 +4,4 @@ import UIKit
 
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
 var secondStorySentence = 
+var thoStorySentence = "If there is a problem , we will find a solution"
